c1f7f8e946f70428cac949ff7206230556dea768
AmjadEngineering <amjadengnring@outlook.com>
2024-04-19 09:15:14 +0500

    address + phone number upate

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             {/* Address */}
             <div>
               <h3 className="text-lg font-bold mb-2">Main Address:</h3>
-              <p>Road Peer near Christian Graveyard, Lahore</p>
+              <p>Unit 1: Road Peer near Christian Graveyard, Lahore</p>
               <p>Unit 2: Salampat Pura, Lahore</p>
               <p>Unit 3: Sunder Industrial Estate, Lahore</p>
             </div>
@@ -17,7 +17,8 @@ const Footer = () => {
             <div>
               <h3 className="text-lg font-bold mb-2">Contact Information:</h3>
               <p>Email: amjad_eng666@yahoo.com</p>
-              <p>Phone: +923219474620</p>
+              <p>Phone 1: +923219474620</p>
+              <p>Phone 2: +923009474620</p>
             </div>
           </div>
         </div>
